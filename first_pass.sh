dc4ecabca740241e193e1225cbafc90588590d31
Terry Murray <tpcmurray@gmail.com>
2023-12-13 21:46:57 -0330

    Update first_pass.sh

--- first_pass.sh
+++ first_pass.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/bash
-echo "Disabling root password …"
+echo "**** Disabling root password …"
 echo $(passwd -l root)
-echo "Enabling firewall …"
+echo "**** Enabling firewall …"
 echo $(ufw enable)
-echo "Installing OpenSSH …"
+echo "**** Installing OpenSSH …"
 echo $(apt-get install openssh-server)
-echo "Starting SSH service …"
+echo "**** Starting SSH service …"
 echo $(systemctl start sshd)
-echo "Setting correct permissions on shadow file …"
+echo "**** Setting correct permissions on shadow file …"
 echo $(chmod 640 /etc/shadow)
-echo "Enabling stack and heap address space layout randomization  …"
+echo "**** Enabling stack and heap address space layout randomization  …"
 echo $(sysctl -w kernel.randomize_va_space=2 )
-echo "Fetching updated app data …"
+echo "**** Fetching updated app data …"
 echo $(apt update)
-echo "Upgrading apps …"
+echo "**** Upgrading apps …"
 echo $(apt upgrade)
